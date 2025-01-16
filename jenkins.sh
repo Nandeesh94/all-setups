d0c439409001b2cabf90da8e5661bc2ec10d0bee
Nandeesh94 <nandeesh.flipkart@gmail.com>
2025-01-16 21:32:39 +0530

    Update jenkins.sh

--- jenkins.sh
+++ jenkins.sh
@@ -1,5 +1,8 @@
 #STEP-1: INSTALLING GIT JAVA-1.8.0  
-yum install git java-1.8.0-openjdk maven -y
+yum install git java-1.8.0-openjdk -y
+wget https://dlcdn.apache.org/maven/maven-3/3.9.9/binaries/apache-maven-3.9.9-bin.tar.gz
+sudo tar -xvzf apache-maven-3.9.9-bin.tar.gz -C /opt/
+sudo ln -sf /opt/apache-maven-3.9.9/bin/mvn /usr/bin/mvn
 
 #STEP-2: GETTING THE REPO (jenkins.io --> download -- > redhat)
 sudo wget -O /etc/yum.repos.d/jenkins.repo https://pkg.jenkins.io/redhat-stable/jenkins.repo
